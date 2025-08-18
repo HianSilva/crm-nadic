e79267aba31f386ce1fb7e471a50a7093d8ab120
Hian Silva <hiansilacts@gmail.com>
2025-08-18 19:53:06 -0300

    Add login and logout views with corresponding URL patterns and login template

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
+class LoginView(LoginView):
+    template_name = 'accounts/login.html'
+    success_url = '/products/'
+
+class LogoutView(LogoutView):
+    next_page = 'login'
