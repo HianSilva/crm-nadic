6042302840d9bbfb35c38723cbcd9e2106136d0a
Hian Silva <hiansilacts@gmail.com>
2025-08-19 12:38:07 +0000

    Refactor redirect URLs to use reverse_lazy in login and logout views

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
 # Create your views here.
 class LoginView(LoginView):
     template_name = 'accounts/login.html'
-    success_url = '/products/'
+    success_url = reverse_lazy('products-list')
 
 class LogoutView(LogoutView):
-    next_page = 'login'
+    next_page = reverse_lazy('login')
